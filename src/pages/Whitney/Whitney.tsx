7e1b0d90a61929a294909b4b8a872ff122785809
Wwestfall91 <wwestfall91@gmail.com>
2026-04-04 18:40:31 -0400

    Favorite Food

--- src/pages/Whitney/Whitney.tsx
+++ src/pages/Whitney/Whitney.tsx
@@ -4,14 +4,18 @@ function WhitneyPage() {
             <h1>Hello my name is Whitney!</h1>
             <div className="question">
                 <div>My favorite color is: </div>
-                <input type="text" />
+                <input type="text" placeholder="Purple"/>
             </div>
             <div className="question">
                 <div>My favorite animal is: </div>
-                <input type="text" />
+                <input type="text" placeholder="Baby Elephant"/>
             </div>
             <div className="question">
                 <div>My favorite teacher is: </div>
+                <input type="text" placeholder="Miss Hardy"/>
+            </div>
+            <div className="question">
+                <div>My favorite food is: </div>
                 <input type="text" />
             </div>
         </div>
