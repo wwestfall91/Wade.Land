2c7e7dfc0f0cb4fcaf444e359f9a36293b65b514
Wade <wwestfall91@gmail.com>
2026-04-03 20:19:15 -0400

    Preparing for Liftoff

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <button className="dont-click">Dont Click Me</button>
       <div className="container">
-        <div className="welcome-text">I'm glad you're finally here</div>
+        <div className="welcome-text">You found me!</div>
         <button className="enter-button">ENTER</button>
       </div>
     </>
   );
 }
-
-function OnClickDontClick() {}
 
 export default App;
